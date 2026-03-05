37e929d9b71a80942b59b8bb12193ba330839c16
Wellington Gonçalves de Azevedo <wazevedo@vidacare.com.br>
2026-03-05 15:54:53 -0300

    Integração com banco SQL Server e categorias

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AvaliacaoTecnica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AvaliacaoTecnica.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using AvaliacaoTecnica.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 namespace AvaliacaoTecnica
 {
     public class Program
@@ -5,6 +9,9 @@ namespace AvaliacaoTecnica
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
